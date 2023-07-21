8d87d97306b31a76c30b8897dd0decbad1665100
Affaan Kidwai <kidwaiaffaan@gmail.com>
2023-07-21 12:24:10 +0530

    product info page initial commit

--- src/Navigation/ParentNavigation.js
+++ src/Navigation/ParentNavigation.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../Screens/HomeScreen";
+import MyCart from "../Screens/Cart";
+import ProductInfo from "../Screens/ProductInfo";
 
 const Stack = createNativeStackNavigator();
 
 const ParentNavigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+      {/* <Stack.Screen name="Cart" component={MyCart} /> */}
+      <Stack.Screen name="Product Info" component={ProductInfo} />
     </Stack.Navigator>
   );
 };
