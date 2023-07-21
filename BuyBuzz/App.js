a07fadd56f3a6b99c1467cb8d9178bdaa525e683
Parth442002 <parthkatiyar04@gmail.com>
2023-07-22 01:33:55 +0530

    Removed BuyBuzz ✅

--- BuyBuzz/App.js
+++
@@ -1,9 +0,0 @@
-import { NavigationContainer } from "@react-navigation/native";
-import ParentNavigator from "./src/Navigation/ParentNavigation";
-export default function App() {
-  return (
-    <NavigationContainer>
-      <ParentNavigator />
-    </NavigationContainer>
-  );
-}
